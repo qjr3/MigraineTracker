83fafe23eae1b62d4e7368692d017cbc67276b6e
qjr3 <qjr3@students.uwf.edu>
2015-10-28 20:44:56 -0500

    Added some comments to clarify naming problems...
    Think RESTfully! :)

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -21,14 +21,33 @@ $router->get('test', 'TestController@index');
 
 $router->get('journal', 'JournalController@index');
 
+// Use RESTful methods so later they can be converted to $router->resources('user', 'UserController');
+// $router->get('user/{id}, UserController@show');
 $router->get('user/{id}', 'UserController@showProfile');
 
 // Authentication Routes
+// $router->get('register', 'Auth\AuthController@create');
 $router->get('register', 'Auth\AuthController@getRegister');
+
+// $router->post('register', 'Auth\AuthController@store');
 $router->post('register', 'Auth\AuthController@postRegister');
 
-$router->get('login', 'Auth\AuthController@getLogin');
+// $router->get('login', 'Auth\AuthController@showLogin');
+$router->get('login', 'Auth\AuthController@getLogin'); // get? what is a login? I don't know this object type.
+
+// $router->post('login', 'Auth\AuthController@login');
 $router->post('login', 'Auth\AuthController@postLogin');
+
+/* 
+// Doesn't require call to controller. Simplest method is typically the best method.
+$router->get('logout', function() {
+    Auth::logout();
+    return redirect('/');
+});
+
+// or
+// $router->get('logout', 'Auth\AuthController@logout'); // why call it get? you're not getting anything in terms of code.
+*/
 $router->get('logout', 'Auth\AuthController@getLogout');
 
 //$router->get('user/login',              'UserController@login');
