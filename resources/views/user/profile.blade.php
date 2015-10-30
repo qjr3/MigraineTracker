7ab346577e3ea1a319460e222c1d307a83374639
John Slay <johnslayweb@gmail.com>
2015-10-29 21:28:13 -0500

    Working on profile design

--- resources/views/user/profile.blade.php
+++ resources/views/user/profile.blade.php
@@ -4,12 +4,23 @@
 
 @section('content')
 
-<div class="col-md-12"><h1>{{ $user->name }}</h1></div>
-<div class="col-md-4">
-	<h3>Account Info</h3>
+<div class="col-md-2">
+	<h3>Personal Info</h3>
 	<hr>
+	<div class="clearfix">
+		<strong><p class="text-primary pull-left">Username:</p> <div class="pull-right">{{ $user->name }}</div></strong>
+	</div>
+	<div class="clearfix">
+		<strong><p class="text-primary pull-left">First Name:</p> <div class="pull-right">{{ $user->first_name }}</div></strong>
+	</div>
+	<div class="clearfix">
+		<strong><p class="text-primary pull-left">Last Name:</p> <div class="pull-right">{{ $user->last_name }}</div></strong>
+	</div>
+	<div class="clearfix">
+		<strong><p class="text-primary pull-left">Email:</p> <div class="pull-right">{{ $user->email }}</div></strong>
+	</div>
 </div>
-<div class="col-md-8">
+<div class="col-md-10">
 	<h3>General Info</h3>
 	<hr>
 </div>
