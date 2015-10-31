57fc62ab266654d056c17c2d06f19e3a5192b6e9
Quinn <>
2015-10-31 18:43:28 +0000

    clean up forms a bit more.....added user auth (still incomplete)

--- resources/views/journal/create.blade.php
+++ resources/views/journal/create.blade.php
@@ -13,22 +13,23 @@
 @section('content')
 {!! Form::open(['action' =>'JournalController@store', 'method' => 'post'])  !!}
 <div class="form-group">
-    {!! Form::label('Name', 'Name',['class' => 'form-label'] ) !!}
-    {!! Form::text('name', null, ['class' => 'form-control']) !!}
+  <!--  {!! Form::label('Name', 'Name',['class' => 'form-label'] ) !!} -->
+    {!! Form::text('name', null, ['class' => 'form-control', 'placeholder' => 'Journal Name']) !!}
 </div>
 
 <div class="form-group">
-    {!! Form::label('description', 'Description:', ['class' => 'form-label']) !!}
-    {!! Form::textarea('description', null, ['class' => 'form-control']) !!}
+  <!--  {!! Form::label('description', 'Description:', ['class' => 'form-label']) !!} -->
+    {!! Form::textarea('description', null, ['class' => 'form-control', 'placeholder' => 'Journal Description']) !!}
 </div>
 <div class="form-group">
-    {!! Form::label('location', 'Location', ['class' => 'form-label']) !!}
-    {!! Form::text('location', null, ['class' => 'form-control']) !!}
+   <!-- {!! Form::label('location', 'Location', ['class' => 'form-label']) !!} -->
+    {!! Form::text('location', null, ['class' => 'form-control', 'placeholder' => 'Location']) !!}
 </div>
 <div class="form-group">
     {!! Form::label('severity', 'Severity:', ['class' => 'form-label']) !!}
     {!! Form::select('severity',
         [
+            '' => '',
             '1' => '1',
             '2' => '2',
             '3' => '3',
@@ -43,17 +44,14 @@
         null, ['class' => 'form-control']) !!}
 </div>
 <div class="form-group">
-    {!! Form::label('location', 'Location:', ['class' => 'form-label']) !!}
-    {!! Form::text('location', null, ['class' => 'form-control']) !!}
-</div>
-<div class="form-group">
-    {!! Form::label('weather', 'Weather:', ['class' => 'form-label']) !!}
-    {!! Form::text('weather', null, ['class' => 'form-control']) !!}
+    <!-- {!! Form::label('weather', 'Weather:', ['class' => 'form-label']) !!} -->
+    {!! Form::text('weather', null, ['class' => 'form-control', 'placeholder' => 'Weather']) !!}
 </div>
 <div class="form-group">
     {!! Form::label('noise_level', 'Noise Level:', ['class' => 'form-label']) !!}
     {!! Form::select('noise_level',
         [
+             '' => '',
             '1' => '1',
             '2' => '2',
             '3' => '3',
@@ -86,68 +84,68 @@
          null, ['class' => 'form-control']) !!}
 </div>
 <div class="form-group">
-    {!! Form::label('triggers_id', 'Triggers:', ['class' => 'form-label']) !!}
-    {!! Form::text('name', null, ['class' => 'form-control', 'disabled' => 'true']) !!}
+    <!-- {!! Form::label('triggers_id', 'Triggers:', ['class' => 'form-label']) !!} -->
+    {!! Form::text('name', null, ['class' => 'form-control', 'disabled' => 'true', 'placeholder' => 'Triggers List : WiP']) !!}
 </div>
 <div class="form-group">
-    {!! Form::label('medicines_id', 'Medicines:', ['class' => 'form-label']) !!}
-    {!! Form::text('medicines_id', null, ['class' => 'form-control', 'disabled' => 'true']) !!}
+   <!-- {!! Form::label('medicines_id', 'Medicines:', ['class' => 'form-label']) !!} -->
+    {!! Form::text('medicines_id', null, ['class' => 'form-control', 'disabled' => 'true', 'placeholder' => 'Medicine List : WiP']) !!}
 </div>
 <div class="form-group">
-    {!! Form::label('still_suffering', 'still_suffering:', ['class' => 'form-label']) !!}
-    {!! Form::select('still_suffering', [ '' => '', 'True' => 'true', 'False' => 'false'], null, ['class' => 'form-control']) !!}
+    {!! Form::label('still_suffering', 'Are you currently suffering?', ['class' => 'form-label']) !!}
+    {!! Form::select('still_suffering', [ '' => '', 'true' => 'Yes', 'false' => 'No'], null, ['class' => 'form-control']) !!}
 </div>
 <div class="form-group">
-    {!! Form::label('start_time', 'Start Time:', ['class' => 'form-label']) !!}
-    {!! Form::text('start_time', null, ['class' => 'form-control', 'disabled' => 'true']) !!}
+   <!-- {!! Form::label('start_time', 'Start Time:', ['class' => 'form-label']) !!} -->
+    {!! Form::text('start_time', null, ['class' => 'form-control', 'disabled' => 'true', 'placeholder' => 'Start-Time : WiP']) !!}
 </div>
 <div class="form-group">
-    {!! Form::label('end_time', 'End Time:', ['class' => 'form-label']) !!}
-    {!! Form::text('end_time', null , ['class' => 'form-control', 'disabled' => 'true']) !!}
+   <!-- {!! Form::label('end_time', 'End Time:', ['class' => 'form-label']) !!} -->
+    {!! Form::text('end_time', null , ['class' => 'form-control', 'disabled' => 'true', 'placeholder' => 'End-Time : WiP']) !!}
 </div>
 <div class="form-group">
-    {!! Form::label('has_aura', 'Has Aura:', ['class' => 'form-label']) !!}
-    {!! Form::select('has_aura', [ '' => '', 'true' => 'True', 'false' => 'False'], null, ['class' => 'form-control']) !!}
+    {!! Form::label('has_aura', 'Are you experiencing any auras?', ['class' => 'form-label']) !!}
+    {!! Form::select('has_aura', [ '' => '', 'true' => 'Yes', 'false' => 'No'], null, ['class' => 'form-control']) !!}
 </div>
 <div class="form-group">
-    {!! Form::label('aura_description', 'aura_description:', ['class' => 'form-label']) !!}
-    {!! Form::text('aura_description', null, ['class' => 'form-control']) !!}
+   <!-- {!! Form::label('aura_description', 'aura_description:', ['class' => 'form-label']) !!} -->
+    {!! Form::textarea('aura_description', null, ['class' => 'form-control', 'placeholder' => 'Description of Aura']) !!}
 </div>
 <div class="form-group">
-    {!! Form::label('has_nausea', 'Nauseous:', ['class' => 'form-label']) !!}
-    {!! Form::select('has_nausea',  [ '' => '', 'true' => 'True', 'false' => 'False'], null, ['class' => 'form-control']) !!}
+    {!! Form::label('has_nausea', 'Are you nauseous?', ['class' => 'form-label']) !!}
+    {!! Form::select('has_nausea',  [ '' => '', 'true' => 'Yes', 'false' => 'No'], null, ['class' => 'form-control']) !!}
 </div>
 <div class="form-group">
-    {!! Form::label('has_vomitted', 'Vomitted:', ['class' => 'form-label']) !!}
-    {!! Form::select('has_vomitted',  [ '' => '', 'true' => 'True', 'false' => 'False'], null, ['class' => 'form-control']) !!}
+    {!! Form::label('has_vomitted', 'Have you vomitted?', ['class' => 'form-label']) !!}
+    {!! Form::select('has_vomitted',  [ '' => '', 'true' => 'Yes', 'false' => 'No'], null, ['class' => 'form-control']) !!}
 </div>
 <div class="form-group">
-    {!! Form::label('has_light_sensativity', 'Light Sensative?', ['class' => 'form-label']) !!}
-    {!! Form::select('has_light_sensativity',  [ '' => '', 'true' => 'True', 'false' => 'False'], null, ['class' => 'form-control']) !!}
+    {!! Form::label('has_light_sensativity', 'Are you experiencing sensativity to light?', ['class' => 'form-label']) !!}
+    {!! Form::select('has_light_sensativity',  [ '' => '', 'true' => 'Yes', 'false' => 'No'], null, ['class' => 'form-control']) !!}
 </div>
 <div class="form-group">
-    {!! Form::label('has_sound_sensativity', 'Sound Sensative:', ['class' => 'form-label']) !!}
-    {!! Form::select('has_sound_sensativity',  [ '' => '', 'true' => 'True', 'false' => 'False'], null, ['class' => 'form-control']) !!}
+    {!! Form::label('has_sound_sensativity', 'Are you experiencing sensativity to sounds?', ['class' => 'form-label']) !!}
+    {!! Form::select('has_sound_sensativity',  [ '' => '', 'true' => 'Yes', 'false' => 'No'], null, ['class' => 'form-control']) !!}
 </div>
 <div class="form-group">
-    {!! Form::label('has_disrupted', 'Been Disrupted:', ['class' => 'form-label']) !!}
-    {!! Form::select('has_disrupted',  [ '' => '', 'true' => 'True', 'false' => 'False'], null, ['class' => 'form-control']) !!}
+    {!! Form::label('has_disrupted', 'Are you being disrupted?', ['class' => 'form-label']) !!}
+    {!! Form::select('has_disrupted',  [ '' => '', 'true' => 'Yes', 'false' => 'No'], null, ['class' => 'form-control']) !!}
 </div>
 <div class="form-group">
-    {!! Form::label('missed_workschool', 'Missed Work or School?', ['class' => 'form-label']) !!}
-    {!! Form::select('missed_workschool',  [ '' => '', 'true' => 'True', 'false' => 'False'], null, ['class' => 'form-control']) !!}
+    {!! Form::label('missed_workschool', 'Did you miss work or school?', ['class' => 'form-label']) !!}
+    {!! Form::select('missed_workschool',  [ '' => '', 'true' => 'Yes', 'false' => 'No'], null, ['class' => 'form-control']) !!}
 </div>
 <div class="form-group">
-    {!! Form::label('missed_routines', 'Missed Other Routines?', ['class' => 'form-label']) !!}
-    {!! Form::select('missed_routines',  [ '' => '', 'true' => 'True', 'false' => 'False'], null, ['class' => 'form-control']) !!}
+    {!! Form::label('missed_routines', 'Have you missed other routines?', ['class' => 'form-label']) !!}
+    {!! Form::select('missed_routines',  [ '' => '', 'true' => 'Yes', 'false' => 'No'], null, ['class' => 'form-control']) !!}
 </div>
 <div class="form-group">
     {!! Form::label('social_plans', 'Social Plans:', ['class' => 'form-label']) !!}
-    {!! Form::text('social_plans', null, ['class' => 'form-control', 'disabled' => 'true']) !!}
+    {!! Form::text('social_plans', null, ['class' => 'form-control', 'disabled' => 'true', 'placeholder' => 'WiP']) !!}
 </div>
 <div class="form-group">
     {!! Form::label('activities', 'Activities:', ['class' => 'form-label']) !!}
-    {!! Form::text('activities', null, ['class' => 'form-control', 'disabled' => 'true']) !!}
+    {!! Form::text('activities', null, ['class' => 'form-control', 'disabled' => 'true', 'placeholder' => 'WiP']) !!}
 </div>
 <div class='form-group'>
     {!! Form::submit('Submit') !!}
