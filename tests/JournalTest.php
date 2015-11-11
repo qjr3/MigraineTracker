1783fd58cf0412412c2f6a1eef303ea9bb8acde4
johnslay <johnslayweb@gmail.com>
2015-11-11 15:43:57 -0600

    Added some tests

--- tests/JournalTest.php
+++ tests/JournalTest.php
@@ -1,36 +1,36 @@
 <?php
-
-use Illuminate\Foundation\Testing\WithoutMiddleware;
-use Illuminate\Foundation\Testing\DatabaseMigrations;
-use Illuminate\Foundation\Testing\DatabaseTransactions;
-
-class JournalTest extends TestCase
-{
-    /**
-     * A basic test example.
-     *
-     * @return void
-     */
-    public function testExample()
-    {
-        $this->assertTrue(true);
-    }
-    
-    public function createEmptyJournal()
-    {
-        $journal = new Journal;
-        $this->assertFalse($journal->save());
-    }
-    
-    public function viewNonJournal()
-    {
-        $this->get('/journal/0')->see('Whoops, looks like something went wrong.');
-    }
-    
-    public function viewValidJournal()
-    {
-        $this->get('/journal/1')->see('Migraine Tracker');
-    }
-    
-    
-}
+//
+//use Illuminate\Foundation\Testing\WithoutMiddleware;
+//use Illuminate\Foundation\Testing\DatabaseMigrations;
+//use Illuminate\Foundation\Testing\DatabaseTransactions;
+//
+//class JournalTest extends TestCase
+//{
+//    /**
+//     * A basic test example.
+//     *
+//     * @return void
+//     */
+//    public function testExample()
+//    {
+//        $this->assertTrue(true);
+//    }
+//
+//    public function createEmptyJournal()
+//    {
+//        $journal = new Journal;
+//        $this->assertFalse($journal->save());
+//    }
+//
+//    public function viewNonJournal()
+//    {
+//        $this->get('/journal/0')->see('Whoops, looks like something went wrong.');
+//    }
+//
+//    public function viewValidJournal()
+//    {
+//        $this->get('/journal/1')->see('Migraine Tracker');
+//    }
+//
+//
+//}
