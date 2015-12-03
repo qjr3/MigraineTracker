7343a09d42a8807b7c99f7f92f2614ffbeac6ece
John Slay <johnslayweb@gmail.com>
2015-12-02 19:00:33 -0600

    More touch up

--- resources/views/journal/show.blade.php
+++ resources/views/journal/show.blade.php
@@ -10,38 +10,45 @@
 @stop
 
 @section('content')
-<div class='panel panel-info'>
-    <div class='panel-heading'>
-        <div class="row">
-            <div class="col-xs-8 h3">{{ $journal->name }}</div>
-            <div class='col-xs-2 h3'>{!! link_to_action('JournalController@edit', 'Edit' , $journal->id, ['class'=>'btn btn-warning btn-block']) !!}</div>
-            <div class='col-xs-2 h3'>
-                {!! Form::open( ['route' => ['journal.destroy', $journal], 'method' => 'delete']) !!}
-                <button type="submit" class="btn btn-danger btn-block">Delete</button>
-                {!! Form::close() !!}
+    <div class='panel panel-default'>
+        <div class='panel-heading'>
+            <div class="row">
+                <div class="col-xs-8 h3">{{ $journal->name }}</div>
+                <div class='col-xs-2 h3'>{!! link_to_action('JournalController@edit', 'Edit' , $journal->id, ['class'=>'btn btn-warning btn-block']) !!}</div>
+                <div class='col-xs-2 h3'>
+                    {!! Form::open( ['route' => ['journal.destroy', $journal], 'method' => 'delete']) !!}
+                    <button type="submit" class="btn btn-danger btn-block">Delete</button>
+                    {!! Form::close() !!}
+                </div>
+            </div>
+            <div class='row'>
+                <div class='col-xs-12 h4'><em>{!! nl2br($journal->description) !!}</em></div>
             </div>
         </div>
-        <div class='row'>
-            <div class='col-xs-12 h4'><em>{!! nl2br($journal->description) !!}</em></div>
-        </div>
-    </div>
-    <div class='panel-body'>
-        <table class="table table-striped">
-            <tbody>
-                <tr>
+        <div class='panel-body'>
+            <table class="table table-striped">
+                <tbody>
+
                 @if(!empty($journal->start_time) && !empty($journal->end_time))
-                    <th>Duration</th>
-                    <td class="text-center ">{{ $duration }}</td>
+                    <tr>
+                        <th>Duration</th>
+                        <td class="text-center ">{{ $duration }}</td>
+                    </tr>
                 @else
                     @if(!empty($journal->start_time) && empty($journal->end_time))
-                        <th>Time Started</th>
-                        <td class="text-center ">{{ $journal->start_time }}</td>
+                        <tr>
+                            <th>Time Started</th>
+                            <td class="text-center ">{{ $journal->start_time }}</td>
+                        </tr>
                     @elseif(!empty($journal->start_time) && empty($journal->end_time))
-                        <th>Time Ended</th>
-                        <td class="text-center ">{{ $journal->end_time }}</td>
+                        <tr>
+                            <th>Time Ended</th>
+                            <td class="text-center ">{{ $journal->end_time }}</td>
+                        </tr>
                     @endif
+
                 @endif
-                </tr>
+
                 <tr>
                     <th>Severity</th>
                     <td class="text-center ">{{ $journal->severity }}</td>
@@ -51,25 +58,26 @@
                     <td class='text-center {{ !empty($journal->has_disrupted) ? ($journal->has_disrupted=='true' ? ' danger' : ' success') : ' warning' }}'>{{ !empty($journal->has_disrupted) ? ($journal->has_disrupted=='true' ? 'Yes' : 'No') : '' }}</td>
                 </tr>
                 <tr>
-                <tr>
                     <th>Light Level</th>
                     <td class='text-center'>{{ $journal->light_level }}</td>
                 </tr>
+                <tr>
                     <th>Light Sensitivity</th>
                     <td class='text-center {{ !empty($journal->has_light_sensitivity) ? ($journal->has_light_sensitivity=='true' ? ' danger' : ' success') : ' warning' }}'>{{ !empty($journal->has_light_sensitivity) ? ($journal->has_light_sensitivity=='true' ? 'Yes' : 'No') : '' }}</td>
                 </tr>
-                <tr>
+
                 <tr>
                     <th>Sound Level</th>
                     <td class='text-center'>{{ $journal->sound_level }}</td>
                 </tr>
+                <tr>
                     <th>Sound Sensitivity</th>
                     <td class='text-center {{ !empty($journal->has_sound_sensitivity) ? ($journal->has_sound_sensitivity=='true' ? ' danger' : ' success') : ' warning' }}'>{!! !empty($journal->has_sound_sensitivity) ? ($journal->has_sound_sensitivity=='true' ? 'Yes' : 'No') : '' !!}  </td>
                 </tr>
                 <tr>
                     <th>Nauseous</th>
                     <td class='text-center {{ !empty($journal->has_nausea) ? ($journal->has_nausea=='true' ? ' danger' : ' success') : ' warning' }}'>
-                        {!! !empty($journal->has_nausea) ? ($journal->has_nausea=='true' ? 'Yes' : 'No') : '' !!}  
+                        {!! !empty($journal->has_nausea) ? ($journal->has_nausea=='true' ? 'Yes' : 'No') : '' !!}
                     </td>
                 </tr>
                 <tr>
@@ -81,20 +89,20 @@
                 <tr>
                     <th>Missed Work or School</th>
                     <td class='text-center {{ !empty($journal->missed_workschool) ? ($journal->missed_workschool=='true' ? ' danger' : ' success') : ' warning' }}'>
-                        {!! !empty($journal->missed_workschool) ? ($journal->missed_workschool=='true' ? 'Yes' : 'No') : '' !!}  
+                        {!! !empty($journal->missed_workschool) ? ($journal->missed_workschool=='true' ? 'Yes' : 'No') : '' !!}
                     </td>
                 </tr>
                 <tr>
                     <th>Missed Other Activities</th>
                     <td class='text-center {{ !empty($journal->missed_routines) ? ($journal->missed_routines=='true' ? ' danger' : ' success') : ' warning' }}'>
-                        {!! !empty($journal->missed_routines) ? ($journal->missed_routines=='true' ? 'Yes' : 'No') : '' !!}  
+                        {!! !empty($journal->missed_routines) ? ($journal->missed_routines=='true' ? 'Yes' : 'No') : '' !!}
                     </td>
                 </tr>
-            </tbody>
-        </table>
-    </div>
+                </tbody>
+            </table>
+        </div>
 
-</div>
+    </div>
 @stop
 
 @section('footer')
--- resources/views/notes/create.blade.php
+++ resources/views/notes/create.blade.php
@@ -1,22 +1,31 @@
 @extends('master')
 
 @section('content')
+    
+    <div class='panel panel-default'>
+        <div class='panel-heading'>
+            <h4>New Note</h4>
+        </div>
+        <div class='panel-body'>
+            {!! Form::open(['action' =>'NoteController@store', 'method' => 'post']) !!}
+            <!-- Name Form Input -->
+            <div class="form-group">
+                {!! Form::label('name', 'Note Name:', ['class' => 'form-label']) !!}
+                {!! Form::text('name', null, ['class' => 'form-control']) !!}
+            </div>
+            <!-- Description Form Input -->
+            <div class="form-group">
+                {!! Form::label('body', 'Note Body:', ['class' => 'form-label']) !!}
+                {!! Form::textarea('body', null, ['class' => 'form-control']) !!}
+            </div>
 
-{!! Form::open(['action' =>'NoteController@store', 'method' => 'post']) !!}
-<h4>Add Note</h4>
-<!-- Name Form Input -->
-<div class="form-group">
-    {!! Form::label('name', 'Note Name:', ['class' => 'form-label']) !!}
-    {!! Form::text('name', null, ['class' => 'form-control']) !!}
-</div>
-<!-- Description Form Input -->
-<div class="form-group">
-    {!! Form::label('body', 'Note Body:', ['class' => 'form-label']) !!}
-    {!! Form::textarea('body', null, ['class' => 'form-control']) !!}
-</div>
-<div class="text-right">
-    {!! Form::submit('Add Note', ['class' => 'btn btn-info']) !!}
-</div>
-{!! Form::close() !!}
+            {!! Form::close() !!}
+        </div>
+        <div class="panel-footer">
+            <div class="text-right">
+                {!! Form::submit('Add Note', ['class' => 'btn btn-info']) !!}
+            </div>
+        </div>
+    </div>
 
 @stop
